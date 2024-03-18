a3f2de27fe2c8d26271c8f415df16add3e7925b5
TrioTech3 <03trio.tech@gmail.com>
2024-03-18 14:28:20 +0530

    Init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using FoodAPI.DataAccess;
+using FoodAPI.InterFace;
+using FoodAPI.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IUser, UserBAL>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
